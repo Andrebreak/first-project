f56a324baded5797c00a988bd32517ac0cc63bb6
Andreas <andreasnonfon2@gmail.com>
2025-06-15 20:28:17 +0100

    new files and folders

--- app/api/send-Mail/route.ts
+++ app/api/send-Mail/route.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import nodemailer from 'nodemailer';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Méthode non autorisée' });
   }
